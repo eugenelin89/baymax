0615c5df9b594bb864ed42f7b3a0cf7581afe680
Eugene Lin <eugenelin89@gmail.com>
2017-01-21 18:21:21 -0500

    init

---
+++ baymax_firebase/index.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();              // get an instance of the express Router
+var admin = require("firebase-admin");
+
+
+var serviceAccount = require("./baymax_key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://baymax-fraser.firebaseio.com"
+});
+
+var db = admin.database();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'Got Bot??' });    
+});
+
+// ROUTES FOR OUR API
+// router.route('/theRoute') returns an instance of a single route which you can then use to handle HTTP verbs with optional middleware. 
+// =============================================================================
+router.route('/message')
+    .get(function(req,res){
+        // test code
+        //var ref = db.ref("baymax/12345");
+        //var node = ref.child("messages");
+        //node.set({test:'test'});
+        res.json({ message: 'Hello Bot!' });   
+    })
+
+    .post(function(req,res){
+        // incoming message
+        // outgoing message
+        var body = req.body;
+        console.log('received: '+JSON.stringify(body));
+        var ref = db.ref("messages/"+body["sender_id"]+"/");
+        ref.push(body); 
+        res.json(body);
+});
+
+
+// Express Config
+// ==============
+app.set('port', (process.env.PORT || 5000));
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.all(path, callback [, callback ...])
+// This method is like the standard app.METHOD() methods, except it matches all HTTP verbs.
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // To allow cross-origin so front-end dev can be done on a different box.
+    //http://stackoverflow.com/questions/12111936/angularjs-performs-an-options-http-request-for-a-cross-origin-resource
+    //res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods",'PUT,GET,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", ['content-type','X-Chop-User-Session-Token','x-parse-application-id','x-parse-rest-api-key']);
+    next();    
+});
+
+// Handle Error
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.send(500, 'FAIL...');
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/v1', router);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
---
+++ baymax_fraser/app.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+from flask import Flask, request, make_response, abort, logging
+import urllib, json, os, sys, requests, tasks
+# _access_token and _post_msg_url will eventually be moved to another module/process for sending messages.
+
+#########
+# Setup #
+#########
+app = Flask(__name__)
+
+#########
+# Flask #
+#########
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    abort(401)
+
+@app.route("/test", methods=['GET'])
+def test():
+    tasks.add.delay(1,2)
+    return "Good Test!"
+
+
+##########
+# API.AI #
+##########
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    # Get incoming message
+    sender_id = req.get("originalRequest").get("data").get("sender").get("id")
+    sender_msg = req.get("originalRequest").get("data").get("message").get("text")
+    response_msg = req.get("result").get("fulfillment").get("speech")
+    timestamp = req.get("timestamp")
+    tasks.store_dialog.delay(timestamp, sender_id, sender_msg, response_msg)
+    if "smalltalk" in req.get("result").get("action"):
+        speech = req.get("result").get("fulfillment").get("speech")
+        return {"speech": speech, "displayText":speech }
+    return {}
+
+#########################
+# Old FB Messenger Code #
+#########################
+
+#  @app.route("/fb_webhook/<bot_id>", methods=['GET'])
+#  def handshake(bot_id):
+#      debug('Hello FooBar!')
+#      debug(request.data)
+#      token = request.args.get('hub.verify_token')
+#      challenge = request.args.get('hub.challenge')
+#      if token == os.environ['VERIFY_TOKEN'] and challenge != None: # need fix
+#          return challenge
+#      else:
+#          abort(401)
+
+#_post_msg_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='+os.environ['FBOT_ACCESS_TOKEN']
+test = 0
+def testfunc(data):
+    global test
+    test = test + 1
+    sender_id = data['entry'][0]['messaging'][0]['sender']['id']
+    resp_data = {
+        "recipient" : {"id":sender_id},
+        "message" : {"text":"TEST -> "+str(test)}        
+    }
+    post_result = requests.post(_post_msg_url, json=resp_data)
+    return post_result
+
+@app.route("/fb_webhook/<bot_id>", methods=['POST'])
+def process_message(bot_id):
+    # received message from user
+    debug('Process message...\n'+request.data)
+    data = request.json # type dict, whereas request.data is type str
+    tasks.process.delay(data)
+    return "ok"
+
+
+###########
+# Helpers #
+###########
+
+
+def debug(message):
+    print(message, file=sys.stderr)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
